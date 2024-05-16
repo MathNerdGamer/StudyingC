fae6edb8550fb4c3ff7db89ab06dbd4d86eb0a91
MathNerdGamer <mathnerd@anothermathblog.com>
2024-05-16 05:33:08 -0500

    Hello, world!

--- hello_world/hello_world.c
+++ hello_world/hello_world.c
@@ -1,8 +1,3 @@
-# Chapters 1 + 2
-
-```c
-// hello_world.c
-// "Hello, world!"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,4 +6,3 @@ int main(int argc, char **argv)
     printf("Hello, world!\n");
     return EXIT_SUCCESS;
 }
-```
