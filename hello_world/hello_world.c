14c644217bbab5e2db8a90731bce807ea5517119
MathNerdGamer <mathnerd@anothermathblog.com>
2024-05-20 04:13:13 -0500

    Chapters 1 + 2 completed.

--- hello_world/hello_world.c
+++ hello_world/hello_world.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
+int main(int const argc, [[maybe_unused]] char const **argv)
 {
     printf("Hello, world!\n");
+
+    for( int i = 1; i < argc; i++ )
+    {
+        printf("Hello, %s!\n", argv[i]);
+    }
+
     return EXIT_SUCCESS;
 }
